d265792d3e3b2b39bfcc38647492bd6cba5dc923
angel-torres <alas4224@gmail.com>
2020-03-21 23:24:17 -0500

    ls

--- client/src/components/UpdateDialog.tsx
+++ client/src/components/UpdateDialog.tsx
@@ -6,7 +6,6 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import axios from 'axios';
 
 // COMPONENTS
